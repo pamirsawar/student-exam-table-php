2f8f6d7226192b004dbfcf6a665f6fc61ff241f7
samir pawar <samirpwar1@gmail.com>
2022-02-18 11:09:36 +0530

    first commit

---
+++ examination.sql
@@ -0,0 +1,148 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 18, 2022 at 06:31 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `examination`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `result`
+--
+
+CREATE TABLE `result` (
+  `id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `marks` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `result`
+--
+
+INSERT INTO `result` (`id`, `student_id`, `subject_id`, `marks`) VALUES
+(1, 2, 2, 55),
+(2, 2, 3, 80),
+(3, 3, 3, 50),
+(4, 3, 1, 55),
+(5, 3, 2, 88);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `name`) VALUES
+(1, 'samir'),
+(2, 'jyoti'),
+(3, 'partho');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `name`) VALUES
+(1, 'maths'),
+(2, 'english'),
+(3, 'science');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `result`
+--
+ALTER TABLE `result`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `subject_id` (`subject_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `result`
+--
+ALTER TABLE `result`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `result`
+--
+ALTER TABLE `result`
+  ADD CONSTRAINT `result_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`),
+  ADD CONSTRAINT `result_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+
+//database config
+$host = "localhost";
+$user = "root"; 
+$password = ""; 
+$dbname = "examination"; 
+date_default_timezone_set("Asia/Calcutta");
+
+
+$con = mysqli_connect($host, $user, $password,$dbname);
+if (!$con) {
+ die("Connection failed: " . mysqli_connect_error());
+}
+
+// $sql="select stu.name as student, sub.name as subject, result.marks as mark from students as stu, subjects as sub, result where result.student_id=stu.id and result.subject_id=sub.id ";
+
+$sql="select stu.name as student, sub.name as subjects, result.marks as mark from students stu inner join result on result.student_id=stu.id 
+inner join subjects sub on result.subject_id=sub.id";
+
+$result=$con->query($sql);
+
+$arr=array();
+
+$student=array();
+$subject=array();
+$mark=array();
+// echo $con->error;
+while($row = $result->fetch_assoc()){
+
+    array_push($student, $row['student']);
+    array_push($subject, $row['subjects']);
+    array_push($mark, $row['mark']);
+
+    if (!isset($arr[$row['student']])) {
+        $arr[$row['student']]['rowspan'] = 0;
+    }
+    $arr[$row['student']]['printed'] = 'no';
+    $arr[$row['student']]['rowspan'] += 1;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<style>
+
+table{
+    /* border:1px solid black; */
+}
+tbody tr td{
+    border-bottom: 1px solid black;
+}
+
+</style>
+<body>
+
+<table >
+
+<thead>
+    <tr>
+        <th>Student name</th>
+        <th>Subject</th>
+        <th>Marks</th>
+    </tr>
+</thead>
+<tbody>
+    <?php
+    
+    for($i=0; $i < sizeof($subject); $i++) {
+        $student_name = $student[$i];
+        echo "<tr>";
+        if ($arr[$student_name]['printed'] == 'no') {
+            echo "<td rowspan='".$arr[$student_name]['rowspan']."'>".$student_name."</td>";
+            $arr[$student_name]['printed'] = 'yes';
+        }
+        echo "<td>".$subject[$i]."</td>";
+        echo "<td>".$mark[$i]."</td>";
+        echo "</tr>";
+    }
+    ?>
+</tbody>
+
+</table>
+
+
+</body>
+</html>
